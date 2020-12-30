28a507efbbaee3dcefa2ee372ed1826ef49813f1
Angus Wu <s0937196597@gmail.com>
2020-12-30 16:54:36 +0800

    reset shopindex basic

--- src/views/shop/Index.vue
+++ src/views/shop/Index.vue
@@ -1,5 +1,28 @@
 <template>
-    <v-app>
-        <v-col class="pt-0">test</v-col>
-    </v-app>
+  <v-app>
+    <v-col class="pt-0">test</v-col>
+  </v-app>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      products: {
+        data: [],
+      },
+    };
+  },
+  methods: {
+    getProducts() {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/products/all`;
+      const vm = this;
+      this.$http.get(api).then((response) => {
+        vm.products.data = response.data.products;
+      });
+    },
+  },
+  created() {
+      this.getProducts();
+  },
+};
+</script>
