91caada904c6b396c5dfe8ac1b1fbb847c6853b6
Angus Wu <s0937196597@gmail.com>
2021-01-02 20:36:00 +0800

    add footer

--- src/views/shop/Index.vue
+++ src/views/shop/Index.vue
@@ -79,6 +79,23 @@
           </v-col>
         </v-row>
       </v-container>
+      <v-footer v-bind="localAttrs" padless>
+        <v-card flat tile width="100%" class="blue-grey darken-1 text-center">
+          <v-card-text>
+            <v-btn v-for="icon in footerIcons" :key="icon" class="mx-4" icon>
+              <v-icon size="24px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
@@ -100,6 +117,12 @@ export default {
           disableOnInteraction: true,
         },
       },
+      footerIcons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
       products: {
         data: [],
       },
