71842673f6680b8c2d5f142dd4b079eeae164ca9
Angus Wu <s0937196597@gmail.com>
2021-02-24 17:56:59 +0800

    add products banner area

--- src/views/shop/Products.vue
+++ src/views/shop/Products.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
-    Products
+    <v-img
+      src="../../assets/banner-products.jpg"
+      max-height="460"
+      position="center 20%"
+    >
+    </v-img>
+    <v-container fluid>
+    </v-container>
   </div>
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    // 取得所有商品資料
+    getProducts() {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/products/all`;
+      const vm = this;
+      this.$http.get(api).then((response) => {
+        console.log(response.data);
+        vm.products = response.data.products;
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
 };
 </script>
