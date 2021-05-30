7efd54e8b96958bec6845d1708a18d87952053aa
Angus Wu <s0937196597@gmail.com>
2021-05-31 00:32:32 +0800

    add banner

--- src/views/shop/Products.vue
+++ src/views/shop/Products.vue
@@ -6,7 +6,37 @@
       position="center 20%"
     >
     </v-img>
-    <v-container fluid>
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+          <v-card class="mx-auto">
+            <v-list>
+              <v-list-group
+                v-for="item in items"
+                :key="item.title"
+                v-model="item.active"
+                :prepend-icon="item.action"
+                no-action
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                  </v-list-item-content>
+                </template>
+
+                <v-list-item v-for="child in item.items" :key="child.title">
+                  <v-list-item-content>
+                    <v-list-item-title v-text="child.title">
+                      <v-btn></v-btn>
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-group>
+            </v-list>
+          </v-card>
+        </v-col>
+        <v-col cols="9">main area</v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -15,6 +45,30 @@ export default {
   data() {
     return {
       products: [],
+      sidebar: [
+        {
+          text: '所有商品',
+          icon: 'mdi-shopping',
+        },
+      ],
+      items: [
+        {
+          action: 'mdi-ticket',
+          active: true,
+          items: [{ title: 'List Item' }],
+          title: 'Attractions',
+        },
+        {
+          action: 'mdi-silverware-fork-knife',
+          active: true,
+          items: [
+            { title: 'Breakfast & brunch' },
+            { title: 'New American' },
+            { title: 'Sushi' },
+          ],
+          title: 'Dining',
+        },
+      ],
     };
   },
   methods: {
