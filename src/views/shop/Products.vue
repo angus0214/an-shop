762d5af1fc6aa8b850a735d8b8aed7887d9e57d5
Angus Wu <s0937196597@gmail.com>
2021-06-01 23:41:07 +0800

    products : add product card UI

--- src/views/shop/Products.vue
+++ src/views/shop/Products.vue
@@ -35,7 +35,37 @@
             </v-list>
           </v-card>
         </v-col>
-        <v-col cols="9">main area</v-col>
+        <v-col cols="9">
+          <v-row>
+            <v-col cols="4">
+              <v-card>
+                <v-img
+                  height="200"
+                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                ></v-img>
+                <v-card-title>Cafe Badilico</v-card-title>
+                <v-card-text>$556</v-card-text>
+                <v-card-actions>
+                  <v-btn color="deep-purple lighten-2" text @click="reserve">
+                    詳細資訊
+                  </v-btn>
+                  <v-btn color="deep-purple lighten-2" text @click="reserve">
+                    加入購物車
+                  </v-btn>
+                </v-card-actions>
+                <v-divider class="mx-4"></v-divider>
+                <v-card-title>分類標籤</v-card-title>
+                <v-card-text>
+                  <v-chip-group column>
+                    <v-chip dark color="blue-grey lighten-2">5:30PM</v-chip>
+                    <v-chip dark color="blue-grey lighten-2">7:30P</v-chip>
+                    <v-chip dark color="blue-grey lighten-2">8:00PM</v-chip>
+                  </v-chip-group>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
   </div>
