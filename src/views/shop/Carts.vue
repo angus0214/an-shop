5050e1f066a44fcd6e4559c771175c922345a38b
Angus Wu <s0937196597@gmail.com>
2021-06-11 21:20:01 +0800

    carts : set step1 UI

--- src/views/shop/Carts.vue
+++ src/views/shop/Carts.vue
@@ -10,30 +10,146 @@
       <v-row>
         <v-col cols="12">
           <v-stepper v-model="stepEl">
-            <v-stepper-header>
-              <v-stepper-step step="1" :complete="stepEl > 1">
+            <v-stepper-header class="px-9">
+              <v-stepper-step
+                step="1"
+                :complete="stepEl > 1"
+                color="blue-grey lighten-1"
+              >
                 確認商品
               </v-stepper-step>
               <v-divider></v-divider>
-              <v-stepper-step step="2" :complete="stepEl > 2">
+              <v-stepper-step
+                step="2"
+                color="blue-grey lighten-1"
+                :complete="stepEl > 2"
+              >
                 填寫資料
               </v-stepper-step>
               <v-divider></v-divider>
-              <v-stepper-step step="3" :complete="stepEl > 3">
+              <v-stepper-step
+                step="3"
+                color="blue-grey lighten-1"
+                :complete="stepEl > 3"
+              >
                 完成訂購
               </v-stepper-step>
             </v-stepper-header>
             <v-stepper-items>
               <v-stepper-content step="1">
-                <v-card color="grey lighten-1" class="mb-12" height="200px"
-                  >111111</v-card
-                >
-                <v-btn color="primary" @click="stepEl = 2">
-                  Continue
-                </v-btn>
-                <v-btn text>
-                  Cancel
-                </v-btn>
+                <div class="d-flex justify-center">
+                  <v-card flat class="my-6" width="80%">
+                    <v-divider></v-divider>
+                    <v-simple-table>
+                      <template v-slot:default>
+                        <thead>
+                          <tr>
+                            <th class="text-left"></th>
+                            <th class="text-left text-h6 font-weight-bold">
+                              品名
+                            </th>
+                            <th class="text-left text-h6 font-weight-bold">
+                              售價
+                            </th>
+                            <th class="text-left text-h6 font-weight-bold">
+                              數量
+                            </th>
+                            <th class="text-left"></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(product, index) in carts" :key="index">
+                            <td class="pa-2">
+                              <v-img
+                                max-height="90"
+                                max-width="90"
+                                :src="product.product.imageUrl"
+                              ></v-img>
+                            </td>
+                            <td>{{ product.product.title }}</td>
+                            <td>{{ product.product.price }}</td>
+                            <td>
+                              <v-btn-toggle active-class="none">
+                                <v-btn text>
+                                  <v-icon>mdi-chevron-left</v-icon>
+                                </v-btn>
+                                <v-btn text>
+                                  {{ product.qty }}
+                                </v-btn>
+                                <v-btn text @click="countQty(product.qty)">
+                                  <v-icon>mdi-chevron-right</v-icon>
+                                </v-btn>
+                              </v-btn-toggle>
+                            </td>
+                            <td>
+                              <v-btn
+                                icon
+                                color="black"
+                                @click="delCart(product.id)"
+                              >
+                                <v-icon size="16">mdi-close</v-icon>
+                              </v-btn>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </template>
+                    </v-simple-table>
+                    <v-divider></v-divider>
+                    <v-row class="mt-5">
+                      <v-col cols="6">
+                        <div
+                          class="rounded pa-3 coupon-tip blue-grey--text text--darken-2 mb-4"
+                        >
+                          現在輸入優惠碼 「AnShop2021」 即可享有 9 折優惠
+                        </div>
+                        <div>
+                          <v-text-field
+                            v-model="message"
+                            type="text"
+                            color="blue-grey"
+                            dense
+                            outlined
+                            append-outer-icon="mdi-send"
+                            placeholder="輸入優惠碼"
+                          >
+                          </v-text-field>
+                        </div>
+                      </v-col>
+                      <v-col cols="6">
+                        <v-row>
+                          <v-col cols="6" class="text-right text-subtitle-2"
+                            >總計金額</v-col
+                          >
+                          <v-col cols="6" class="text-right text-subtitle-2">$ 10000</v-col>
+                          <v-col cols="6" class="text-right text-subtitle-2"
+                            >優惠折抵</v-col
+                          >
+                          <v-col cols="6" class="text-right text-subtitle-2">-$ 10999</v-col>
+                          <v-col cols="6" class="text-right text-subtitle-1 blue-grey--text"
+                            >應付金額</v-col
+                          >
+                          <v-col cols="6" class="text-right text-subtitle-1 blue-grey--text">$ 100000</v-col>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                    <v-divider></v-divider>
+                    <div class="d-flex justify-space-between mt-4">
+                      <v-btn
+                        depressed
+                        color="blue-grey lighten-5"
+                        to="/products"
+                        >繼續購物</v-btn
+                      >
+                      <v-btn
+                        dark
+                        depressed
+                        color="blue-grey lighten-1"
+                        @click="stepEl = 2"
+                        >下一步</v-btn
+                      >
+                    </div>
+                  </v-card>
+                </div>
               </v-stepper-content>
               <v-stepper-content step="2">
                 <v-card color="grey lighten-1" class="mb-12" height="200px"
@@ -69,7 +185,42 @@ export default {
   data() {
     return {
       stepEl: 1,
+      carts: [],
     };
+  },
+  methods: {
+    getCarts() {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/cart`;
+      const vm = this;
+      this.$http.get(api).then((response) => {
+        vm.carts = response.data.data.carts;
+      });
+    },
+    countQty(item) {
+      item = item + 1;
+      console.log(item);
+    },
+  },
+  created() {
+    this.getCarts();
   },
 };
 </script>
+<style>
+.v-stepper__header {
+  box-shadow: none !important;
+}
+.coupon-tip {
+  background-color: #cfd8dc;
+  position: relative;
+}
+.coupon-tip::before {
+  content: '';
+  position: absolute;
+  width: 5px;
+  height: 100%;
+  left: 1px;
+  top: 0;
+  background-color: #78909c;
+}
+</style>
