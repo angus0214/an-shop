0cb5ed3f3c8e497195bffc8c92d5f4a5dce3ab72
Angus Wu <s0937196597@gmail.com>
2021-06-12 15:54:22 +0800

    carts : add step2 / step 3 UI

--- src/views/shop/Carts.vue
+++ src/views/shop/Carts.vue
@@ -104,7 +104,6 @@
                         </div>
                         <div>
                           <v-text-field
-                            v-model="message"
                             type="text"
                             color="blue-grey"
                             dense
@@ -120,15 +119,25 @@
                           <v-col cols="6" class="text-right text-subtitle-2"
                             >總計金額</v-col
                           >
-                          <v-col cols="6" class="text-right text-subtitle-2">$ 10000</v-col>
+                          <v-col cols="6" class="text-right text-subtitle-2"
+                            >$ 10000</v-col
+                          >
                           <v-col cols="6" class="text-right text-subtitle-2"
                             >優惠折抵</v-col
                           >
-                          <v-col cols="6" class="text-right text-subtitle-2">-$ 10999</v-col>
-                          <v-col cols="6" class="text-right text-subtitle-1 blue-grey--text"
+                          <v-col cols="6" class="text-right text-subtitle-2"
+                            >-$ 10999</v-col
+                          >
+                          <v-col
+                            cols="6"
+                            class="text-right text-subtitle-1 blue-grey--text"
                             >應付金額</v-col
                           >
-                          <v-col cols="6" class="text-right text-subtitle-1 blue-grey--text">$ 100000</v-col>
+                          <v-col
+                            cols="6"
+                            class="text-right text-subtitle-1 blue-grey--text"
+                            >$ 100000</v-col
+                          >
                         </v-row>
                       </v-col>
                     </v-row>
@@ -152,26 +161,162 @@
                 </div>
               </v-stepper-content>
               <v-stepper-content step="2">
-                <v-card color="grey lighten-1" class="mb-12" height="200px"
-                  >22222</v-card
-                >
-                <v-btn color="primary" @click="stepEl = 3">
-                  Continue
-                </v-btn>
-                <v-btn text>
-                  Cancel
-                </v-btn>
+                <div class="d-flex justify-center">
+                  <v-card flat width="80%">
+                    <v-form>
+                      <v-row>
+                        <v-col cols="6">
+                          <div
+                            class="font-weight-bold blue-grey--text text-subtitle-1 mb-1"
+                          >
+                            收件人姓名
+                          </div>
+                          <v-text-field
+                            outlined
+                            dense
+                            append-icon="mdi-account"
+                            color="blue-grey lighten-2"
+                            hint="請確認輸入姓名與身分證上一致"
+                          >
+                          </v-text-field>
+                        </v-col>
+                        <v-col cols="6">
+                          <div
+                            class="font-weight-bold blue-grey--text text-subtitle-1 mb-1"
+                          >
+                            收件人電話
+                          </div>
+                          <v-text-field
+                            outlined
+                            dense
+                            append-icon="mdi-phone"
+                            color="blue-grey lighten-2"
+                            hint="屆時貨運人員會以此號碼聯絡取貨"
+                          ></v-text-field
+                        ></v-col>
+                        <v-col cols="6">
+                          <div
+                            class="font-weight-bold blue-grey--text text-subtitle-1 mb-1"
+                          >
+                            Email
+                          </div>
+                          <v-text-field
+                            outlined
+                            dense
+                            append-icon="mdi-email"
+                            color="blue-grey lighten-2"
+                          ></v-text-field
+                        ></v-col>
+                        <v-col cols="6"
+                          ><div
+                            class="font-weight-bold blue-grey--text text-subtitle-1 mb-1"
+                          >
+                            收件地址
+                          </div>
+                          <v-text-field
+                            outlined
+                            dense
+                            append-icon="mdi-map-marker"
+                            color="blue-grey lighten-2"
+                          ></v-text-field
+                        ></v-col>
+                        <v-col cols="12">
+                          <div
+                            class="font-weight-bold blue-grey--text text-subtitle-1 mb-1"
+                          >
+                            備註
+                          </div>
+                          <v-textarea
+                            outlined
+                            color="blue-grey lighten-2"
+                            name="input-7-4"
+                          ></v-textarea
+                        ></v-col>
+                      </v-row>
+                    </v-form>
+                    <v-divider></v-divider>
+                    <div class="d-flex justify-space-between mt-4">
+                      <v-btn
+                        depressed
+                        color="blue-grey lighten-5"
+                        @click="stepEl = 1"
+                        >返回</v-btn
+                      >
+                      <v-btn
+                        dark
+                        depressed
+                        color="blue-grey lighten-1"
+                        @click="stepEl = 3"
+                        >下一步</v-btn
+                      >
+                    </div>
+                  </v-card>
+                </div>
               </v-stepper-content>
               <v-stepper-content step="3">
-                <v-card color="grey lighten-1" class="mb-12" height="200px"
-                  >33333</v-card
-                >
-                <v-btn color="primary" @click="stepEl = 3">
-                  Continue
-                </v-btn>
-                <v-btn text>
-                  Cancel
-                </v-btn>
+                <div class="d-flex justify-center">
+                  <v-card flat width="80%">
+                    <v-expansion-panels class="mb-5">
+                      <v-expansion-panel>
+                        <v-expansion-panel-header
+                          color="blue-grey lighten-4"
+                          class="font-weight-bold blue-grey--text text--darken-4"
+                        >
+                          詳細訂單內容
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content class="py-2">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam, quis
+                          nostrud exercitation ullamco laboris nisi ut aliquip
+                          ex ea commodo consequat.
+                        </v-expansion-panel-content>
+                      </v-expansion-panel>
+                    </v-expansion-panels>
+                    <v-divider></v-divider>
+
+                    <div
+                      class="pl-4 text-h5 blue-grey--text text--darken-2 font-weight-bold my-4"
+                    >
+                      訂購人資訊
+                    </div>
+
+                    <v-row>
+                      <v-col cols="6" class="pl-7 font-weight-bold"
+                        >訂購人姓名</v-col
+                      >
+                      <v-col cols="6">訂購人</v-col>
+                      <v-col cols="6" class="pl-7 font-weight-bold"
+                        >訂購人電話</v-col
+                      >
+                      <v-col cols="6">0931000566</v-col>
+                      <v-col cols="6" class="pl-7 font-weight-bold"
+                        >訂購人 Email</v-col
+                      >
+                      <v-col cols="6">google@gmail.com</v-col>
+                      <v-col cols="6" class="pl-7 font-weight-bold"
+                        >訂購人地址</v-col
+                      >
+                      <v-col cols="6">XXX</v-col>
+                    </v-row>
+                    <v-divider></v-divider>
+                    <div class="d-flex justify-space-between mt-4">
+                      <v-btn
+                        depressed
+                        color="blue-grey lighten-5"
+                        @click="stepEl = 1"
+                        >修改資料</v-btn
+                      >
+                      <v-btn
+                        dark
+                        depressed
+                        color="blue-grey lighten-1"
+                        @click="stepEl = 3"
+                        >確認付款</v-btn
+                      >
+                    </div>
+                  </v-card>
+                </div>
               </v-stepper-content>
             </v-stepper-items>
           </v-stepper>
@@ -184,7 +329,7 @@
 export default {
   data() {
     return {
-      stepEl: 1,
+      stepEl: 3,
       carts: [],
     };
   },
