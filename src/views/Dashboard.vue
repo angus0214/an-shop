81037c6855e2a446acc1c1ce7f0ca01537b41afd
Angus Wu <s0937196597@gmail.com>
2021-06-27 18:09:49 +0800

    dashboard : install vue-chartjs

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        Dashboard
-    </div>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="4"> <LineChart /></v-col>
+        <v-col></v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+<script>
+import LineChart from '../components/LineChart.vue';
+export default {
+  components: { LineChart },
+  data() {
+    return {};
+  },
+};
+</script>
