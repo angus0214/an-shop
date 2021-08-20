6cf1405b7337fb899db969f0b8dd8fea12f67397
Angus Wu <s0937196597@gmail.com>
2021-08-20 21:51:25 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
     disableHostCheck: true
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/an-shop/'
+    : '/'
 }
