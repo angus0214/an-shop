cc5247f73a6ec885fe7663eefad32fb21c16b88b
Angus Wu <s0937196597@gmail.com>
2021-08-28 10:05:00 +0800

    add vuetify custome sass variables / modify container max-width

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import colors from 'vuetify/lib/util/colors'
-import 'vuetify/dist/vuetify.min.css'
+// import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
